607325f4d0acb5ff1b68b89f78d400f0720d549f
GianoglioEnrico <enrico.gianoglio@ibm.com>
2021-09-11 16:03:11 +0200

    Modified Input Control

--- step1.js
+++ step1.js
@@ -1,4 +1,3 @@
-// const step1 = document.getElementById("step_1");
 const attendee = document.getElementById("attendee_container");
 const step1Result = document.getElementById("step1_result");
 let attendeesInput = [];
@@ -13,22 +12,22 @@ export function handleChangeSelect(e) {
   step1Result.style.visibility = "hidden";
   step1Result.style.top = 0;
   step1Height = "120px";
-
+  step2.style.opacity = 0.6;
+  step2.disabled = true;
   // If the selected element is not the default value ("0")
   if (e.target.value !== "0") {
     for (let i = 1; i < attendee.children.length - 1; i++) {
       if (i <= e.target.value) {
-        step1Height = 120 + 80 * e.target.value + "px";
+        step1Height = 120 + 60 * e.target.value + "px";
         attendee.style.height = step1Height;
-
         attendee.children[i].style.visibility = "visible";
         attendeesInput.push(attendee.children[i].children[1]);
+
         // Attendee input event
         attendee.children[i].children[1].addEventListener("keyup", () => {
           let filledInputs = attendeesInput.filter(
             (attendee) => attendee.value !== ""
           );
-
           // If all visible input fields are filled show green check
           if (filledInputs.length === attendeesInput.length) {
             step2.disabled = false;
@@ -36,11 +35,10 @@ export function handleChangeSelect(e) {
             step1Result.innerHTML = doneIcon;
             step1Result.style.visibility = "visible";
             step1Result.style.top = "90%";
-            step1Height = "500px";
+            attendee.style.height = parseInt(step1Height) + 60 + "px";
           }
           // if not all visible attendee inputs are filled
           else {
-            step1Height = 120 + attendeesInput.length * 80 + "px";
             step2.style.opacity = 0.6;
             step2.disabled = true;
             step1Result.style.visibility = "hidden";
